7c4a924d93b18ed0399a7a479d371a8af9e6651a
Mykola Vshyvkov <mvshyvk@gmail.com>
2020-08-08 16:13:50 +0300

    Added invocation of service status handler when hitting /serviceStatus endpoint

--- service/KaldiService/src/main/java/com/mvshyvk/kaldi/service/api/impl/ServiceStatusApiServiceImpl.java
+++ service/KaldiService/src/main/java/com/mvshyvk/kaldi/service/api/impl/ServiceStatusApiServiceImpl.java
@@ -2,6 +2,8 @@ package com.mvshyvk.kaldi.service.api.impl;
 
 import com.mvshyvk.kaldi.service.api.*;
 import com.mvshyvk.kaldi.service.api.NotFoundException;
+import com.mvshyvk.kaldi.service.models.ServiceStatus;
+import com.mvshyvk.kaldi.service.webapp.KaldiServiceAppContext;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
@@ -10,7 +12,8 @@ import javax.ws.rs.core.SecurityContext;
 public class ServiceStatusApiServiceImpl extends ServiceStatusApiService {
 	@Override
 	public Response serviceStatusGet(SecurityContext securityContext) throws NotFoundException {
-		// do some magic!
-		return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+		
+		ServiceStatus status = KaldiServiceAppContext.serviceStatusHandler.getServiceStatus();
+		return Response.ok().entity(status).build();
 	}
 }
